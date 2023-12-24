fbd83a8a907eadb4c7cab77f9de979bff6a5804c
lf888nmdd <2052163735@qq.com>
2023-12-24 17:06:47 +0800

    pages.js update,push pages/userInfo | taber

---
+++ src/pages/userInfo/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <view class="content">
+      <text>123</text>
+    </view>
+  </template>
+  
+  <script setup lang="ts">
+  import { reactive,onMounted  } from 'vue'
+  import {tt} from '../../utils/Interfaces'
+  
+  interface yy {
+    title:string
+  }
+  onMounted(() => {
+    console.log('*******')
+    tt().then(res=>{
+      console.log('1------------123',res)
+    })
+  })
+  
+  const title:yy = reactive({
+      title:'456789999000'
+  }) as any
+  </script>
+  
+  <style>
+
+  </style>
